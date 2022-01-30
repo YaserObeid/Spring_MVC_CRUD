fceb95ca5c4692843c0547345049decd1366ebd8
YaserObeid <yaser.obeid@hotmail.com>
2022-01-30 15:44:33 +0100

    update controller class

--- src/com/obeid/springdemo/controller/CustomerController.java
+++ src/com/obeid/springdemo/controller/CustomerController.java
@@ -1,16 +1,32 @@
 package com.obeid.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.obeid.springdemo.dao.CustomerDAO;
+import com.obeid.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	
+	// inject a customerDAO
+	@Autowired
+	private CustomerDAO customerDAO;
 	
 	@RequestMapping("/list")
-	public String customerList() {
+	public String customerList(Model model) {
+		
+		// get customer from dao
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add customer list to the model
+		model.addAttribute("customers", customers);
+		
 		return "customer-list";
 	}
 }
