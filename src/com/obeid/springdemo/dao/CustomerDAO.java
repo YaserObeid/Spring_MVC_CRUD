5d0a16ce82cade81cee105a4e57137f224693200
YaserObeid <yaser.obeid@hotmail.com>
2022-01-30 15:05:21 +0100

    create dao implimetation classes

---
+++ src/com/obeid/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,13 @@
+package com.obeid.springdemo.dao;
+
+import java.util.List;
+
+import com.obeid.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+		
+	
+
+}
---
+++ src/com/obeid/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,40 @@
+package com.obeid.springdemo.dao;
+
+import java.util.List;
+import javax.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.obeid.springdemo.entity.Customer;
+
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	// inject session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		// get current session
+		Session currentSession = 
+				sessionFactory.getCurrentSession();
+		
+		// create query to fetch the data
+		// import from 'org.hibernate.query.Query;'
+		Query<Customer> query = 
+				currentSession.createQuery("from Customer", Customer.class);
+		
+		// Execute the query & get c customer list
+		
+		List<Customer> customers = query.getResultList();
+		
+		return customers;
+	}
+
+}
