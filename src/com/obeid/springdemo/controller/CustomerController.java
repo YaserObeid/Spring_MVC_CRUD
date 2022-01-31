7e9ce73a18460b5f43e5d2396ba327a6801d0e56
YaserObeid <yaser.obeid@hotmail.com>
2022-01-31 11:15:45 +0100

    GetMapping

--- src/com/obeid/springdemo/controller/CustomerController.java
+++ src/com/obeid/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.obeid.springdemo.dao.CustomerDAO;
@@ -18,7 +19,8 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	//restrict this method to handle GET requesters
+	@GetMapping("/list")
 	public String customerList(Model model) {
 		
 		// get customer from dao
